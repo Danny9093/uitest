6d7ab87446f619e1067e708cb1709e6ded6c82d5
Daniel Natividad-Alvarez <Daniel@Collegesource.com>
2023-08-07 12:24:14 -0700

    New Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import { NavBarHeader } from "./ui-components";
+import {
+  NavBarHeader2 } from './ui-components';
+
 import { MarketingFooter } from "./ui-components"
 
 function App() {
   return (
     <div className="App">
-      <NavBarHeader />
+       <NavBarHeader2 />
       <MarketingFooter />
     </div>
   );
